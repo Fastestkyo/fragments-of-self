03940d1212085955b98e246e0dd1098af22f51b7
Fastestkyo <fastestkyo@gmail.com>
2025-01-26 19:52:07 +0530

    asd

---
+++ scenes/sprites/player/player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+var vely
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	var move_dir = 0
+	if Input.is_action_pressed("ui_left"):
+		move_dir = -1
+	elif Input.is_action_pressed("ui_right"):
+		move_dir = 1
+	else:
+		move_dir = 0 
+	
+	if move_dir != 0:
+		velocity.x = move_dir * SPEED
+	else:
+		velocity.x = 0
+		
+	
+	
+	
+	move_and_slide() 
