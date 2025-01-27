c08011f73b32e0d49fd928565863b17379c16889
Fastestkyo <fastestkyo@gmail.com>
2025-01-27 16:20:06 +0530

    asd

--- scenes/sprites/player/player.gd
+++ scenes/sprites/player/player.gd
@@ -1,35 +1,39 @@
 extends CharacterBody3D
-@onready var anim: AnimationPlayer = $AnimationPlayer
-@onready var spr: Sprite3D = $Sprite3D
+@onready var anim: AnimatedSprite3D = $AnimatedSprite3D
 
-
-const SPEED = 5.0
+const SPEED = 5
 const JUMP_VELOCITY = 4.5
-var vely
+const grav = 25
+var dir
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += -(grav  * delta)
+		anim.play('jump')
+	else:
+		if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"):
+			anim.play('walk')
+		else:
+			anim.play('idle')
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
-	var move_dir = 0
+
 	if Input.is_action_pressed("ui_left"):
-		move_dir = -1
-		spr.flip_h = false
-		anim.current_animation = 'walk'
+		dir = -1
+		anim.flip_h = false
 	elif Input.is_action_pressed("ui_right"):
-		move_dir = 1
-		spr.flip_h = true
-		anim.current_animation = 'walk'
+		dir = 1
+		anim.flip_h = true
 	else:
-		move_dir = 0
-		anim.current_animation = '[stop]'
-		
-	if move_dir != 0:
-		velocity.x = move_dir * SPEED
+		dir = 0
+
+	move_and_slide()
+	moving()
+	
+
+func moving():
+	if dir != 0:
+		velocity.x = dir * SPEED
 	else:
 		velocity.x = 0
-		
-	move_and_slide() 
